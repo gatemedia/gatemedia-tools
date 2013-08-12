4bb036f6bcff6de9fcf4e9449668bfd3e6e36fce
Mike Andrzejewski <mike.aski@gatemedia.ch>
2013-08-12 15:39:22 +0200

    Add use_local_gems binary

--- lib/gatemedia/tools/gems.rb
+++ lib/gatemedia/tools/gems.rb
@@ -1,5 +1,24 @@
 require 'colored'
 
+GATEMEDIA_GEMS_ROOT = ENV['GATEMEDIA_GEMS_ROOT'] || "/Users/#{ENV['USER']}/Code/GateMedia"
+GATEMEDIA_PUBLIC_GEMS_ROOT = ENV['GATEMEDIA_PUBLIC_GEMS_ROOT'] || "#{GATEMEDIA_GEMS_ROOT}/public"
+GATEMEDIA_PRIVATE_GEMS_ROOT = ENV['GATEMEDIA_PRIVATE_GEMS_ROOT'] || "#{GATEMEDIA_GEMS_ROOT}/private"
+
+puts "GateMedia gems root @ #{GATEMEDIA_GEMS_ROOT}".blue
+puts "  public  gems root @ #{GATEMEDIA_PUBLIC_GEMS_ROOT}".blue
+puts "  private gems root @ #{GATEMEDIA_PRIVATE_GEMS_ROOT}".blue
+
+def check_directory_existence directory
+    unless File.directory? directory
+        puts "Gems root directory does not exists! (#{directory})".red
+        exit 1
+    end
+end
+
+check_directory_existence GATEMEDIA_PRIVATE_GEMS_ROOT
+check_directory_existence GATEMEDIA_PUBLIC_GEMS_ROOT
+
+
 module Bundle
     def self.config
         `bundle config`.split "\n"
@@ -18,7 +37,12 @@ module Bundle
         `bundle config --delete local.#{gem_name}`
     end
 
-    def self.use_local gem_name, path
+    def self.use_local gem_name
+        if Gemfile::is_private? gem_name
+            path = "#{GATEMEDIA_PRIVATE_GEMS_ROOT}/#{gem_name}"
+        else
+            path = "#{GATEMEDIA_PUBLIC_GEMS_ROOT}/#{gem_name}"
+        end
         puts "Point '#{gem_name}' to local clone @ #{path}".green
         `bundle config local.#{gem_name} #{path}`
     end
@@ -39,5 +63,39 @@ module Gemfile
 
     def self.gems lines
         lines.keep_if { |line| line.strip.start_with? 'gem ' }
+    end
+
+    def self.is_private? gem_name
+        self.gem_names(self.private_gems(self.lines)).include? gem_name
+    end
+
+    def self.private_gems lines
+        lines.keep_if { |line| /^gem\s+.+@github\.com.+/.match(line) }
+    end
+
+    def self.comment_github_ref gem_name
+        lines = self.lines.collect { |line|
+            if /^# gem '#{gem_name}', git:.*/.match line
+                line[2..-1]
+            elsif /^gem '#{gem_name}', github:.*/.match line
+                "# #{line}"
+            else
+                line
+            end
+        }
+        open('Gemfile', 'w').write lines.join
+    end
+
+    def self.uncomment_github_ref gem_name
+        lines = self.lines.collect { |line|
+            if /^# gem '#{gem_name}', github:.*/.match line
+                line[2..-1]
+            elsif /^gem '#{gem_name}', git:.*/.match line
+                "# #{line}"
+            else
+                line
+            end
+        }
+        open('Gemfile', 'w').write lines.join
     end
 end
