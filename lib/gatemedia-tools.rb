64cdb82f9331b5b8bf2cb09c396584f098f9b497
Mike Andrzejewski <mike.aski@gatemedia.ch>
2013-08-12 11:58:59 +0200

    Fix gem & rake tasks loading

---
+++ lib/gatemedia-tools.rb
@@ -0,0 +1,1 @@
+require 'gatemedia_tools'
--- lib/gatemedia_tools.rb
+++ lib/gatemedia_tools.rb
@@ -2,4 +2,10 @@
 module GatemediaTools
   class Engine < ::Rails::Engine
   end
+
+  class Railtie < ::Rails::Railtie
+    rake_tasks do
+      load 'tasks/stage.rake'
+    end
+  end
 end
