0c3bcb1a3aacc8ff1739e98013ecf2b29588416d
Mike Andrzejewski <mike.aski@gatemedia.ch>
2013-08-12 17:56:57 +0200

    Improve gems source management scripts

--- lib/gatemedia/tools/gems.rb
+++ lib/gatemedia/tools/gems.rb
@@ -9,10 +9,10 @@ puts "  public  gems root @ #{GATEMEDIA_PUBLIC_GEMS_ROOT}".blue
 puts "  private gems root @ #{GATEMEDIA_PRIVATE_GEMS_ROOT}".blue
 
 def check_directory_existence directory
-    unless File.directory? directory
-        puts "Gems root directory does not exists! (#{directory})".red
-        exit 1
-    end
+  unless File.directory? directory
+    puts "Gems root directory does not exists! (#{directory})".red
+    exit 1
+  end
 end
 
 check_directory_existence GATEMEDIA_PRIVATE_GEMS_ROOT
@@ -20,82 +20,207 @@ check_directory_existence GATEMEDIA_PUBLIC_GEMS_ROOT
 
 
 module Bundle
-    def self.config
-        `bundle config`.split "\n"
-    end
 
-    def self.gem_names lines
-        lines.collect { |line| /local\.(.+)/.match(line)[1] }
-    end
+  def self.local_gems
+    self.gem_lines.keep_if { |line| self.is_local line }.collect { |line| self.gem_name line }
+  end
 
-    def self.local_gems lines
-        lines.keep_if { |line| line.start_with? 'local.' }
-    end
+  def self.use_remote gem_name
+    puts "Point '#{gem_name}' to remote repository".red
+    `bundle config --delete local.#{gem_name}`
+puts "Pointed '#{gem_name}' to remote repository...".magenta
+  end
 
-    def self.use_remote gem_name
-        puts "Point '#{gem_name}' to remote repository".red
-        `bundle config --delete local.#{gem_name}`
+  def self.use_local gem_name
+    if Gemfile::is_private? gem_name
+      path = "#{GATEMEDIA_PRIVATE_GEMS_ROOT}/#{gem_name}"
+    else
+      path = "#{GATEMEDIA_PUBLIC_GEMS_ROOT}/#{gem_name}"
     end
+    puts "Point '#{gem_name}' to local clone @ #{path}".green
+    `bundle config local.#{gem_name} #{path}`
+  end
 
-    def self.use_local gem_name
-        if Gemfile::is_private? gem_name
-            path = "#{GATEMEDIA_PRIVATE_GEMS_ROOT}/#{gem_name}"
-        else
-            path = "#{GATEMEDIA_PUBLIC_GEMS_ROOT}/#{gem_name}"
-        end
-        puts "Point '#{gem_name}' to local clone @ #{path}".green
-        `bundle config local.#{gem_name} #{path}`
-    end
+
+  private
+
+  def self.gem_lines
+    `bundle config --no-color`.split "\n"
+  end
+
+  def self.gem_name gem_line
+    /local\.(.+)/.match(gem_line)[1]
+  end
+
+  def self.is_local gem_line
+    gem_line.strip.start_with? 'local.'
+  end
 end
 
+
 module Gemfile
-    def self.lines
-        open('Gemfile').readlines
+
+  def self.github_gems
+    self.gemfile_lines.keep_if { |line|
+        self.is_gem(line) && self.is_from_github(line)
+    }.collect { |line|
+        self.gem_name line
+    }
+  end
+
+  def self.comment_github_ref gem_name
+    dirty = false
+    self.write_sync self.gemfile_lines.collect { |line|
+        new_line = self.toggle_comment line, gem_name, 'git', 'github'
+        dirty = new_line != line
+        new_line
+    }
+    dirty
+  end
+
+  def self.uncomment_github_ref gem_name
+    dirty = false
+    self.write_sync self.gemfile_lines.collect { |line|
+        new_line = self.toggle_comment line, gem_name, 'github', 'git'
+        dirty = new_line != line
+        new_line
+    }
+    dirty
+  end
+
+  def self.is_private? gem_name
+    self.gemfile_lines.keep_if { |line|
+        /gem\s+['"](.+?)['"].+GITHUB_TOKEN/.match line
+    }.collect { |line|
+        self.gem_name line
+    }.include? gem_name
+  end
+
+
+  private
+
+  def self.gemfile_lines
+    open('Gemfile').readlines
+  end
+
+  def self.is_from_github line
+    /gem\s+['"](.+?)['"].+github.+/.match line
+  end
+
+  def self.is_gem line
+    /^gem\s/.match line.strip
+  end
+
+  def self.gem_name line
+    /['"](.+?)['"]/.match(line.strip)[1]
+  end
+
+  def self.toggle_comment line, gem_name, commented, uncommented
+    if /^# gem '#{gem_name}', #{commented}:.*/.match line
+      line[2..-1]
+    elsif /^gem '#{gem_name}', #{uncommented}:.*/.match line
+      "# #{line}"
+    else
+      line
+    end
+  end
+
+  def self.write_sync lines
+    output = open 'Gemfile', 'w'
+    output.sync = true
+    output.write lines.join
+    output.flush
+  end
+end
+
+
+module Gem
+  class Manager
+
+    def use_locals
+      dirties = Gemfile::github_gems.collect { |gem_name|
+        self.use_local gem_name
+      }
+puts ">>>> DIRTIES: #{dirties} / #{dirties.find true}".magenta
+      if dirties.include? true
+puts ">>>> REFRESH BUNDLE".magenta
+        puts `bundle --binstubs`
+else
+puts ">>>> DO NOT REFRESH BUNDLE".magenta
+end
+    end
+  
+    def use_local gem_name
+      dirtyBundler = false
+      unless Bundle::local_gems.include? gem_name
+puts ">>>> #{gem_name} SET TO LOCAL".magenta
+          Bundle::use_local gem_name
+          dirtyBundler = true
+else
+puts ">>>> #{gem_name} ALREADY LOCAL".magenta
+      end
+      
+      dirtyGemfile = false
+      dirtyGemfile = Gemfile::uncomment_github_ref gem_name if Gemfile::is_private? gem_name
+
+      dirtyBundler || dirtyGemfile
     end
 
-    def self.gem_names lines
-        lines.collect { |line| /['"](.+?)['"]/.match(line.strip)[1] }
+    def use_remotes
+      dirtyBundler = false
+      Gemfile::github_gems.collect { |gem_name|
+        if Bundle::local_gems.include? gem_name
+puts ">>>> #{gem_name} SET TO REMOTE".magenta
+          Bundle::use_remote gem_name
+          dirtyBundler = true
+else
+puts ">>>> #{gem_name} ALREADY REMOTE".magenta
+        end
+      }
+
+      dirtyGemfile = false
+      Gemfile::github_gems.collect { |gem_name|
+        dirtyGemfile = Gemfile::comment_github_ref gem_name if Gemfile::is_private? gem_name
+      }
+
+      if dirtyBundler || dirtyGemfile
+puts ">>>> REFRESH BUNDLE".magenta
+        `sync`
+        puts `bundle --binstubs`
+else
+puts ">>>> DO NOT REFRESH BUNDLE".magenta
+end
     end
 
-    def self.github_gems lines
-        lines.keep_if { |line| /gem\s+['"](.+?)['"].+github.+/.match(line.strip) }
+    def use_remotes2
+      dirties = Gemfile::github_gems.collect { |gem_name|
+        self.use_remote gem_name
+      }
+puts ">>>> DIRTIES: #{dirties} / #{dirties.find true}".magenta
+      if dirties.include? true
+puts ">>>> REFRESH BUNDLE".magenta
+        puts `bundle --binstubs`
+else
+puts ">>>> DO NOT REFRESH BUNDLE".magenta
+end
     end
 
-    def self.gems lines
-        lines.keep_if { |line| line.strip.start_with? 'gem ' }
-    end
+    def use_remote gem_name
+      dirtyBundler = false
+      if Bundle::local_gems.include? gem_name
+puts ">>>> #{gem_name} SET TO REMOTE".magenta
+          Bundle::use_remote gem_name
+          dirtyBundler = true
+else
+puts ">>>> #{gem_name} ALREADY REMOTE".magenta
+      end
 
-    def self.is_private? gem_name
-        self.gem_names(self.private_gems(self.lines)).include? gem_name
-    end
+      dirtyGemfile = false
+      dirtyGemfile = Gemfile::comment_github_ref gem_name if Gemfile::is_private? gem_name
 
-    def self.private_gems lines
-        lines.keep_if { |line| /^gem\s+.+@github\.com.+/.match(line) }
+puts ">>>> >>>> PRESS ENTER <<<< <<<<".magenta
+STDIN.getc
+      dirtyBundler || dirtyGemfile
     end
-
-    def self.comment_github_ref gem_name
-        lines = self.lines.collect { |line|
-            if /^# gem '#{gem_name}', git:.*/.match line
-                line[2..-1]
-            elsif /^gem '#{gem_name}', github:.*/.match line
-                "# #{line}"
-            else
-                line
-            end
-        }
-        open('Gemfile', 'w').write lines.join
-    end
-
-    def self.uncomment_github_ref gem_name
-        lines = self.lines.collect { |line|
-            if /^# gem '#{gem_name}', github:.*/.match line
-                line[2..-1]
-            elsif /^gem '#{gem_name}', git:.*/.match line
-                "# #{line}"
-            else
-                line
-            end
-        }
-        open('Gemfile', 'w').write lines.join
-    end
+  end
 end
