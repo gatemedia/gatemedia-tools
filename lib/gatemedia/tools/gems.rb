80b7e684ce2c1908aa0441ee5d1c645fbbf93e9a
Mike Andrzejewski <mike.aski@gatemedia.ch>
2013-08-13 11:02:16 +0200

    Refactor gem sources switching scripts

--- lib/gatemedia/tools/gems.rb
+++ lib/gatemedia/tools/gems.rb
@@ -25,19 +25,22 @@ module Bundle
     self.gem_lines.keep_if { |line| self.is_local line }.collect { |line| self.gem_name line }
   end
 
-  def self.use_remote gem_name
-    puts "Point '#{gem_name}' to remote repository".red
-    `bundle config --delete local.#{gem_name}`
-  end
-
-  def self.use_local gem_name
-    if Gemfile::is_private? gem_name
-      path = "#{GATEMEDIA_PRIVATE_GEMS_ROOT}/#{gem_name}"
-    else
-      path = "#{GATEMEDIA_PUBLIC_GEMS_ROOT}/#{gem_name}"
+  def self.use gem_name, location
+    case location
+    when :local
+      if Gemfile::is_private? gem_name
+        path = "#{GATEMEDIA_PRIVATE_GEMS_ROOT}/#{gem_name}"
+      else
+        path = "#{GATEMEDIA_PUBLIC_GEMS_ROOT}/#{gem_name}"
+      end
+      puts "Point '#{gem_name}' to local clone @ #{path}".green
+      `bundle config local.#{gem_name} #{path}`
+      break
+    when :remote
+      puts "Point '#{gem_name}' to remote repository".red
+      `bundle config --delete local.#{gem_name}`
+      break
     end
-    puts "Point '#{gem_name}' to local clone @ #{path}".green
-    `bundle config local.#{gem_name} #{path}`
   end
 
 
@@ -61,17 +64,17 @@ module Gemfile
 
   def self.github_gems
     self.gemfile_lines.keep_if { |line|
-        self.is_github_gem line
+      self.is_github_gem line
     }.collect { |line|
-        self.gem_name line
+      self.gem_name line
     }
   end
 
   def self.is_private? gem_name
     self.gemfile_lines.keep_if { |line|
-        /gem\s+['"](.+?)['"].+GITHUB_TOKEN/.match line
+      /gem\s+['"](.+?)['"].+GITHUB_TOKEN/.match line
     }.collect { |line|
-        self.gem_name line
+      self.gem_name line
     }.include? gem_name
   end
 
@@ -96,43 +99,31 @@ module Gem
   class Manager
 
     def use_locals
-      dirties = Gemfile::github_gems.collect { |gem_name|
-        self.use_local gem_name
+      refresh_bundle Gemfile::github_gems.collect { |gem_name|
+        point gem_name, :local
       }
-      if dirties.include? true
-        puts "Refresh bundle".magenta
-        puts `bundle --binstubs`
-      end
-    end
-  
-    def use_local gem_name
-      dirtyBundler = false
-      unless Bundle::local_gems.include? gem_name
-          Bundle::use_local gem_name
-          dirtyBundler = true
-      end
-
-      dirtyBundler
     end
 
     def use_remotes
-      dirties = Gemfile::github_gems.collect { |gem_name|
-        self.use_remote gem_name
+      refresh_bundle Gemfile::github_gems.collect { |gem_name|
+        point gem_name, :remote
       }
-      if dirties.include? true
-        puts "Refresh bundle".magenta
-        puts `bundle --binstubs`
+    end
+
+    def point gem_name, location
+      unless Bundle::local_gems.include? gem_name
+        Bundle::use gem_name, location
+        true
+      else
+        false
       end
     end
 
-    def use_remote gem_name
-      dirtyBundler = false
-      if Bundle::local_gems.include? gem_name
-          Bundle::use_remote gem_name
-          dirtyBundler = true
+    def refresh_bundle changes
+      if changes.include? true
+        puts "Refresh bundle".blue
+        puts `bundle --binstubs`
       end
-
-      dirtyBundler
     end
   end
 end
